0e6a6f459229397f867aa144dd68628e83142f55
ASUS <s160418030@student.ubaya.ac.id>
2021-05-18 14:34:55 +0700

    Inisiasi Proyek UAS

---
+++ app/src/main/java/id/ac/ubaya/informatika/alpha160418030_160718006FoodJournalApp/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package id.ac.ubaya.informatika.alpha160418030_160718006FoodJournalApp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.ac.ubaya.informatika.alpha160418030_160718006FoodJournalApp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/id/ac/ubaya/informatika/alpha160418030_160718006FoodJournalApp/view/WelcomeActivity.kt
@@ -0,0 +1,12 @@
+package id.ac.ubaya.informatika.alpha160418030_160718006FoodJournalApp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.ac.ubaya.informatika.alpha160418030_160718006FoodJournalApp.R
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+}

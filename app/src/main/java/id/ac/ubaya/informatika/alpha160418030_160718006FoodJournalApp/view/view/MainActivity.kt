f91abdb7d9a280380f2f46f2ab627f8a9994b5c3
ASUS <s160418030@student.ubaya.ac.id>
2021-06-22 15:04:03 +0700

    Welcome Activity lengkap

--- app/src/main/java/id/ac/ubaya/informatika/alpha160418030_160718006FoodJournalApp/view/view/MainActivity.kt
+++ app/src/main/java/id/ac/ubaya/informatika/alpha160418030_160718006FoodJournalApp/view/view/MainActivity.kt
@@ -2,19 +2,34 @@ package id.ac.ubaya.informatika.alpha160418030_160718006FoodJournalApp.view.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import id.ac.ubaya.informatika.alpha160418030_160718006FoodJournalApp.R
+import id.ac.ubaya.informatika.alpha160418030_160718006FoodJournalApp.model.User
+import id.ac.ubaya.informatika.alpha160418030_160718006FoodJournalApp.viewmodel.ProfileViewModel
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
+    lateinit var userCurr: User
+    private lateinit var viewModel: ProfileViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        viewModel = ViewModelProvider(this).get(ProfileViewModel::class.java)
+
+        var name = intent.getStringExtra(WelcomeActivity.name).toString()
+        var age = intent.getStringExtra(WelcomeActivity.age).toString()
+        var gender = intent.getIntExtra(WelcomeActivity.gender, 1)
+        var height = intent.getStringExtra(WelcomeActivity.height).toString()
+        var weight = intent.getStringExtra(WelcomeActivity.weight).toString()
+        var goal = intent.getStringExtra(WelcomeActivity.goal).toString()
+        userCurr = User(name, age, gender, height, weight, goal)
+        viewModel.newUser(userCurr, false)
 
         navController = Navigation.findNavController(this, R.id.hostFragment)
         NavigationUI.setupActionBarWithNavController(this, navController)
--- app/src/main/java/id/ac/ubaya/informatika/alpha160418030_160718006FoodJournalApp/view/view/WelcomeActivity.kt
+++ app/src/main/java/id/ac/ubaya/informatika/alpha160418030_160718006FoodJournalApp/view/view/WelcomeActivity.kt
@@ -1,12 +1,54 @@
 package id.ac.ubaya.informatika.alpha160418030_160718006FoodJournalApp.view.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
 import id.ac.ubaya.informatika.alpha160418030_160718006FoodJournalApp.R
+import id.ac.ubaya.informatika.alpha160418030_160718006FoodJournalApp.databinding.ActivityWelcomeBinding
+import id.ac.ubaya.informatika.alpha160418030_160718006FoodJournalApp.model.User
 
-class WelcomeActivity : AppCompatActivity() {
+class WelcomeActivity : AppCompatActivity(), ButtonStartClickListener, RadioGenderClickListener, RadioClickListener {
+    private lateinit var dataBinding: ActivityWelcomeBinding
+
+    companion object {
+        val name = "jenengku"
+        val age = "umurku"
+        val gender = ""
+        val height = "dhuwurku"
+        val weight = "bobotku"
+        val goal = "karepku"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+        dataBinding = DataBindingUtil.setContentView(this, R.layout.activity_welcome)
+        dataBinding.user = User("", "", 1, "", "", "")
+        dataBinding.listener = this
+        dataBinding.radiolistener = this
+        dataBinding.radiotarget = this
+
+    }
+
+    override fun onButtonStartClick(v: View, user: User) {
+        var intent = Intent(this, MainActivity::class.java)
+        intent.putExtra(name, user.name)
+        intent.putExtra(age, user.age)
+        intent.putExtra(gender, user.gender)
+        intent.putExtra(height, user.height)
+        intent.putExtra(weight, user.weight)
+        intent.putExtra(goal, user.goal)
+        startActivity(intent)
+
+    }
+
+    override fun onRadioGenderClick(v: View, user: User) {
+        user.gender = v.tag.toString().toInt()
+    }
+
+    override fun onRadioClick(v: View, user: User) {
+        user.goal = v.tag.toString()
     }
 }

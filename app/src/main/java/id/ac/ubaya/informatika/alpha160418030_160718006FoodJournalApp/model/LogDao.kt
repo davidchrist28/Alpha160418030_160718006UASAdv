b68e37ed469e545da575fbba36108b4fc6eb255e
emiliolukito <tgpchannel4@gmail.com>
2021-06-23 14:21:55 +0700

    Perbaikan Query SelectBy Date

--- app/src/main/java/id/ac/ubaya/informatika/alpha160418030_160718006FoodJournalApp/model/LogDao.kt
+++ app/src/main/java/id/ac/ubaya/informatika/alpha160418030_160718006FoodJournalApp/model/LogDao.kt
@@ -13,7 +13,7 @@ interface LogDao {
     @Query("SELECT * FROM foodLogs WHERE id_user = :id_user")
     suspend fun selectLogByUser(id_user: String): List<Log>
 
-    @Query("SELECT date, count(foodName) AS foodName, sum(calories) AS calories FROM foodLogs WHERE id_user = :id_user AND month(date) = month(:date) GROUP BY day(date)")
+    @Query("SELECT logId, id_user, date, count(foodName) AS foodName, sum(calories) AS calories FROM foodLogs WHERE id_user = :id_user AND strftime('%m',date) = strftime('%m',:date) GROUP BY strftime('%d',date)")
     suspend fun selectLogByDate(id_user: String, date: String): List<Log>
 
     @Query("SELECT * FROM foodLogs WHERE logId = :id")
